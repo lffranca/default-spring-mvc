27d589ba0235acab444d619d0465508366f55910
Lucas França <lucas.franca@devolus.com>
2017-02-24 10:34:43 -0300

    Verificando configurações

--- src/main/java/br/com/onlee/spring/controllers/HelloController.java
+++ src/main/java/br/com/onlee/spring/controllers/HelloController.java
@@ -3,11 +3,13 @@ package br.com.onlee.spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/hellocontroller")
 public class HelloController {
 
-	@GetMapping("/hello")
+	@GetMapping()
 	public String hello(Model model) {
 
 		model.addAttribute("name", "John Doe");
